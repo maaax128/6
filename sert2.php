3a531292b5cc5d483094502a9c41edd0ae5a11f5
maaax128 <44034742+maaax128@users.noreply.github.com>
2019-01-27 23:43:16 +0500

    Update sert2.php

--- sert2.php
+++ sert2.php
@@ -1,10 +1,25 @@
 <?php
+session_start();
+//var_dump($_SESSION['name']);
 $sert = imagecreatetruecolor(400, 200);
 $backColor = imagecolorallocate($sert, 255, 255, 230);
+$textColor = imagecolorallocate($sert, 175, 25, 23);
 $image = 'sert.png';
+if (!file_exists($image)) {
+	echo 'Файл не найден';
+	exit;
+
+}
 $imImage = imagecreatefrompng($image);
+$fontFile = __DIR__ . '/arial.ttf';
+$text = 'Вы правильно ответили на все вопросы этого теста!!!';
 imagefill($sert, 0, 0, $backColor);
 imagecopy($sert, $imImage, 0, 0, 0, 0, 400, 200);
+imagettftext($sert, 20, 0, 140, 70, $textColor, $fontFile, $_SESSION['name']); 
+imagettftext($sert, 10, 0, 40, 110, $textColor, $fontFile, $text); 
 header("Content-type: image/png");
 imagepng($sert);
+
+
+
 ?>
